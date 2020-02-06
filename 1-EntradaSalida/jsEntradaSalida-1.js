1fc1309be2a75e8e14498e50855c0b5568dcd6e5
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-06 11:12:59 -0300

    ejercicio uno
    aprendimos alert y variables

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,15 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
-}
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido= "nadie";
 
+	apellido="Aner Scott";
+	alert(apellido);
+
+	//alert recibe una funcion y la muestra//
+	alert("tal cual"); // literal
+	alert(apellido); // alert puede mostrar variables
+	alert("apellido"); // literal x las comillas
+	alert(); // sale vacio
