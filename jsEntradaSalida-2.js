95d71fdc5b0e17933ca74485995a9cbfe48d315b
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-06 12:13:36 -0300

    ejercicio 2

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+
+	var nombre; = prompt("por favor ingrese su nombre", ); // a diferencia del alert el prompt retorna un valor, si no pongo la asignacion de variable a prompt 
+	
+    
+
+
+    
+  console.log  ( "por favor ingrese su nombre " + nombre );
+
+
+}
+
