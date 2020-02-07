84d71166758fdbbe344bca40ea89fb63a6bed457
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-07 11:56:10 -0300

    ejercicio 9
    tomar importe x id y aplicar aumento

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,18 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+var importe;
+var porcentaje;
+var resultado;
+var aumento;
+
+porcentaje=10
+importe=document.getElementById('sueldo').value;
+importe=parseInt(importe);
+aumento=importe*porcentaje/100;
+resultado=importe+aumento;
+
+document.getElementById('resultado').value=resultado;
+
 	
 }
