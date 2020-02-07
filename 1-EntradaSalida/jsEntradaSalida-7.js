8fb1dc62c8adf1d70adf5e3b8b5243d401388d12
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-07 11:12:51 -0300

    ejercicio 7
    usamos id + parseint para operaciones suma resta etc...

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,87 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
 	
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	resultadoDeLaSuma=primerNumero+segundoNumero;
+	
+	alert(resultadoDeLaSuma);
+
+
+
+	
+
+
 }
 
 function restar()
 {
 	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaResta;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+	
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+
+
+
+		resultadoDeLaResta=primerNumero-segundoNumero;
+		alert(resultadoDeLaResta)
+
+
 }
 
 function multiplicar()
 { 
+
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaMultiplicacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+	
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+
+
+		resultadoDeLaMultiplicacion=primerNumero*segundoNumero;
+
+		alert(resultadoDeLaMultiplicacion)
 	
 }
 
 function dividir()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaDivision;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
 	
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);	
+
+
+
+			resultadoDeLaDivision=primerNumero/segundoNumero;
+
+			alert(resultadoDeLaDivision)
+
 }
 
