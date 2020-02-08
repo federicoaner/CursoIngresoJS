adf73f3d2aaef0969c9732492740e9eca8cc7f60
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-08 13:42:07 -0300

    ejercicio 5
    aprendimos id con frase concatenada

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -3,5 +3,27 @@ ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
 	
+	var nombre;
+	var edad;
+	var fraseUno
+	var fraseDos
+    
+	fraseUno= "su nombre es "
+	fraseDos= " y su edad es "
+
+    nombre=document.getElementById('elNombre').value;
+    edad=document.getElementById('laEdad').value;
+
+
+    alert(fraseUno + nombre + fraseDos + edad) ;
+
+
+
+
+
+
+
+
+
 }
 
