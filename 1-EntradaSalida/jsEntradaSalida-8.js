814844fe89bd6449f42d29c96f07798ea438a035
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-08 17:12:01 -0300

    ejercicio 8
    ingresamos los nros como enteros x id, luego realizamos operacion resto

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -5,4 +5,27 @@ ej.: "El resto es 0 ."*/
 function SacarResto()
 {
 	
+    var dividendo;	
+	var divisor;
+	var resto;
+
+	dividendo=document.getElementById('numeroDividendo').value;
+	dividendo=parseInt(dividendo);
+	
+	divisor=document.getElementById('numeroDivisor').value;
+	divisor=parseInt(divisor);
+
+	resto=dividendo%divisor;
+	alert("el resto es: " + resto);
+
+
+
+
+
+
+
+
+
+
+
 }
