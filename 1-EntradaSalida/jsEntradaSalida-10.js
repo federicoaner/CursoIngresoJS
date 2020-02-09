8bc22e10646148bf92b816c4401e3626c61de6a2
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-09 18:56:18 -0300

    ejercicio 10
    porcentaje con descuento

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,25 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+
+
+    var importe;
+	var porcentaje;
+	var resultado;
+	var descuento;
+
+	porcentaje=25;
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+	descuento=importe*porcentaje/100;
+	resultado=importe-descuento;
+
+	document.getElementById('resultado').value=resultado;
+
+
+
+
+
+
 	
 }
