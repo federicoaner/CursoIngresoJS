2aea689fcfed35ffe16b3546dbac88331afdaa73
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-11 21:28:12 -0300

    tp ferrete construccion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,66 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largoTerreno;
+	var anchoTerreno;
+	var perimetro;
+    var	rectangulo;
+
+	largoTerreno=document.getElementById('Largo').value;
+	largoTerreno=parseInt(largoTerreno);
+
+	anchoTerreno=document.getElementById('Ancho').value;
+	anchoTerreno=parseInt(anchoTerreno);
+
+	perimetro=(anchoTerreno+largoTerreno)*2;
+	rectangulo=perimetro*3;
+	alert("la cantidad de alambre que nececitara es de metros: " + rectangulo);
+
+
+
 
 }
 function Circulo () 
 {
+
+	var radio;
+	var perimetroCirculo;
+	var resultadoPerimetro
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+
+	perimetroCirculo= (3.14 * 2) * radio ;
+	resultadoPerimetro=perimetroCirculo * 3;
+	alert("la cantidad de alambre q necesitara es de metros: " + resultadoPerimetro);
 	
 }
 function Materiales () 
 {
+	var area;
+    var largoTerreno;
+	var anchoTerreno;
+	var cementoxMetro;
+	var calxMetro;
+
+	largoTerreno=document.getElementById('Largo').value;
+	largoTerreno=parseInt(largoTerreno);
+
+	anchoTerreno=document.getElementById('Ancho').value;
+	anchoTerreno=parseInt(anchoTerreno);
+
+
+	area= largoTerreno*anchoTerreno;
+
+	cementoxMetro=2
+	calxMetro=3
+	
+
+	bolsasDeCemeto= cementoxMetro  * area;
+	bolsasDeCal= calxMetro * area;
+
+	alert("bolsas de cemento " + bolsasDeCemeto + " bolsas de cal " + bolsasDeCal);
+
+
+
 	
 }
