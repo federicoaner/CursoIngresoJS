bebb7bde0ec9e24c6f88dad1844b95c8a4b1f4e3
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-12 20:09:17 -0300

    punto 3 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var porcentaje;
+	var precioFinal;
+
+	precio=prompt("ingrese el precio: ");
+	descuento=prompt("ingrese el porcentaje de descuento: ");
+
+	porcentaje=precio*descuento/100;
+
+	precioFinal=precio-porcentaje;
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
+
+
+
+
 
 }
