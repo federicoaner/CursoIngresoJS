82ffbcbd70c36660680a4ea004fca57b6dda7f36
federicoaner <60724374+federicoaner@users.noreply.github.com>
2020-02-12 19:42:42 -0300

    punto 1 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("ingrese el ancho: ");
+	largo=prompt("ingrese el alto: ");
+	ancho=parseInt(ancho),
+	largo=parseInt(largo),
+	
+	perimetro=(ancho+largo)*2;
+
+	alert("el perimetro es de: " + perimetro + " metros");
+
 
 }
