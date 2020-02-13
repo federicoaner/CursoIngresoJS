bc893d72aeef3d60730b4adc265ae17d2619cef3
federicoaner <federicoaner@gmail.com>
2020-02-13 19:22:13 -0300

    ejercicio  if

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	var estadoCivil;
+	
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+		if(edad<18&&estadoCivil == soltero)
+	{
+		alert("es muy pequenio NO ser soltero");
+	}
 	
 
 
