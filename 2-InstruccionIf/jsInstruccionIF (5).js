20e45489cd82795b549ed1dcf87ee2fddb36df05
alumno <alumno>
2020-02-13 12:06:10 -0300

    ejercicios if

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	if(edad>17 || edad<13)
+	{		
+	 alert("no adolecente");
+	}
+
+	// if(edad>17)
+	// {		
+	//  alert("no adolecente");
+	// }
+
+	// if(edad<13)
+	// {		
+	//  alert("no adolecente");
+	// }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,27 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	
 
+	 if(edad<13)
+	 {		
+	 alert("es menor");
+	 }
+
+	 
+	 
+    if(edad>17)
+	{		
+	 alert(" es mayor");
+	}
+	else
+	{	
+	 alert("es adolecente");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
