5aabd02d5817e280adbcdf8d9098de0654a452bc
federicoaner <federicoaner@gmail.com>
2020-02-15 17:47:15 -0300

    parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,54 @@
 function mostrar()
 {
 
+	var numUno;
+	var numDos;
+	var suma;
+
+
+
+	numUno=prompt("ingrese un numero: ");
+	numDos=prompt("ingrese otro numero");
+
+	if(numUno==numDos)
+	{
+		alert(numUno+numDos);
+	}
+	else
+	{
+		numUno=parseInt(numUno);
+		numDos=parseInt(numDos);
+		suma=numUno+numDos;
+
+
+		if(numUno>numDos)
+		{
+		alert(numUno-numDos);
+		}
+		else
+		{	
+		alert(numUno+numDos);
+		}
+	}
+	
+		if(suma>10)
+		{
+		alert("la suma es: " + suma + " y supero el 10");
+		}
+	
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
 }
