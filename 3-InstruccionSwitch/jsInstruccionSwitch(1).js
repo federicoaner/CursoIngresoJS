c21f8b6eabef9f0820507438c8ca142fd0a6e209
federicoaner <federicoaner@gmail.com>
2020-02-19 18:11:17 -0300

    ejercicios switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -4,6 +4,42 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 
+switch(mesDelAño)
+{	
+
+	case 1:
+	{
+		alert("que comiences bien el año");
+		break;
+	}
+
+	case 3:
+	{
+		alert("a clases");
+		break;
+	}
+
+	case 7:
+	{
+		alert("se vienen las vacaciones!!! ");
+		break;
+	}
+
+	case 12:
+	{
+		alert("felices fiestas");
+		break;
+	}
+
+
+	document.getElementById('mes').value=mesDelAño;
+
+
+}	
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
