8bc60dae579173e9a4c79422bfdd275462ee5e13
federicoaner <federicoaner@gmail.com>
2020-02-26 20:14:00 -0300

    ejercicios parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -7,9 +7,21 @@ function mostrar()
 	var pesoIngresado;
 	var marcaIngresada;
 	var tempAlmacenamiento;
+	var temperaturaPar;
+	var contadorTempPar;
+	var pesoMaximo;
+	var pesoMinimo;
+	var marcaMasPesado;
+	var contadorBajoCero;
+	var acumulador;
+
 	
 
 	contador=0;
+	contadorPar=0;
+	contadorTempPar=0;
+	contadorBajoCero=0;
+	acumulador=0;
 	
 	
 
@@ -22,7 +34,7 @@ function mostrar()
 
 		pesoIngresado=prompt("por favor ingrese un peso entre 100 y -100: ");
 		pesoIngresado=parseInt(pesoIngresado);
-		
+
 
 	while(pesoIngresado<-100||pesoIngresado>100)
 	{
@@ -39,21 +51,41 @@ function mostrar()
 	{
 		tempAlmacenamiento=prompt("error ingrese temperatura Almacenamiento indicada");
 		tempAlmacenamiento=parseInt(tempAlmacenamiento);
-
 	}
 
+		if(tempAlmacenamiento%2==0)
+		{
+			contadorTempPar++;
+			//temperaturaPar=temperaturaPar+tempAlmacenamiento;
+		}
+		
+		if(contador==1)
+		{
+			pesoMaximo=pesoIngresado;
+			pesoMinimo=pesoIngresado;
+		}
+
+		else
+		{
+			if(pesoIngresado>pesoMaximo)
+			{
+			pesoMaximo=pesoIngresado;
+			marcaMasPesado=marcaIngresada;
+			}
+			else
+			{
+			pesoMinimo=pesoIngresado;
+			}
+
+		}
 		
 
+		if(tempAlmacenamiento<0)
+		{
+			contadorBajoCero++;
+		}
 
-		
-		
-			
-
-		
-		
-		
-		
-
+		acumulador=acumulador+pesoIngresado;
 		
 
 
@@ -61,6 +93,16 @@ function mostrar()
 
 	}//while principal
 
+	promedio=acumulador/contador;
+	document.write("la cant de temp pares es: "+contadorTempPar+"<br>");
+	document.write("el peso maximo es: "+pesoMaximo+"<br>");
+	document.write(marcaMasPesado+"<br>");
+	document.write("la cantidad de productos bajo cero es: "+contadorBajoCero+"<br>");
+	document.write("el promedio es: "+promedio+"<br>");
+	document.write("el peso maximo es: " +pesoMaximo+ "  el peso peso Minimo es: "+pesoMinimo+"<br>");
+
+	//preguntar en clase marcamaximo undefined
+	
 
 
 
