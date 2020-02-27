268fcaa95f4d3d72f200816a8b27d7b8ef40ba49
federicoaner <federicoaner@gmail.com>
2020-02-27 19:37:13 -0300

    ejercicios parcial

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,40 @@
 function mostrar()
 {
+	var numeroUno;
+	var	numeroDos;
+	var division;
+	var suma;
+
+	
+	numeroUno=prompt("ingrese un nro: ");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("ingrese un nro: ");
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		alert(""+numeroUno+""+numeroDos)
+	}
+
+	if(numeroUno>numeroDos)
+	{
+		division=numeroUno/numeroDos;
+		alert(division);
+	}
+	else
+	{
+		suma=numeroUno+numeroDos;
+		alert(suma);
+	}
+
+	if(suma<50)
+	{
+		alert("la suma es "+suma +"  y es menor a 50");
+	}
+
+
+
+
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,40 @@
 function mostrar()
 {
+	var	cantidadBolsas=0;
+	var contadorBolsas=0
+	var	contador=0
+	var kilos;
+	var respuesta="si"
 
+	while(respuesta=="si")
+	{
+
+		contador++;
+		cantidadBolsas=prompt("ingrese cantidad de Bolsas: ");
+		cantidadBolsas=parseInt(cantidadBolsas);
+
+		while(cantidadBolsas>5)
+		{
+			cantidadBolsas=prompt("erro ingrese cantidad de Bolsas: ");
+			cantidadBolsas=parseInt(cantidadBolsas);
+		}
+
+		kilos=prompt("ingrese cantidad de kilos emte 0 y 500: ");
+		kilos=parseInt(kilos);
+
+		while(kilos<0||kilos>500)
+		{
+			kilos=prompt("error ingrese cantidad de kilos correcta : ");
+			kilos=parseInt(kilos);
+
+		}
+
+
+		respuesta=prompt("desea continuar?")
+
+
+
+
+
+	}//while principal
 }
