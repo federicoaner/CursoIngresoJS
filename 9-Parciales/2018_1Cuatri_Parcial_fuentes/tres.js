51f7ed073d3f8c58f681119ff8e0cf82fe52a75c
alumno <alumno>
2020-02-28 15:07:37 -0300

    practica para parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,23 +1,101 @@
 function mostrar()
 {
 	
-	var precio
-	var descuento
-	var precioFinal
-	var porcentaje
+	var kilosxBolsa;
+	var cantidadBolsas;
+	var importexBolsa;
+	var respuesta="si";
+	var cantidadKilos;
+	var marca;
+	var contador=0;
+	var carga=0;
+
+	
+	
+	
+
+	while(respuesta=="si")
+	{
+		contador++;
+		
+
+		marca=prompt("ingrese la marca ");
+
+		kilosxBolsa=prompt("ingrese kilos x bolsa");
+		kilosxBolsa=parseInt=(kilosxBolsa);
+
+		cantidadBolsas=prompt("ingrese cantidad de bolsas")
+
+
+		cantidadkilos=kilosxBolsa*cantidadBolsas
+		carga=carga+cantidadkilos
+		if(carga>1000)
+		{
+			alert("supera la carga maxima");
+		}
+		
+
+
+
+		respuesta=prompt("desea continuar?");
+
+		
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// var precio
+	// var descuento
+	// var precioFinal
+	// var porcentaje
 
 	
 
-	precio=prompt("porfavor ingrese el precio: ");
-	precio=parseInt(precio);
+	// precio=prompt("porfavor ingrese el precio: ");
+	// precio=parseInt(precio);
 
-	descuento=prompt("porfavor ingrese el descuento: ");
-	descuento=parseInt(descuento);
+	// descuento=prompt("porfavor ingrese el descuento: ");
+	// descuento=parseInt(descuento);
 
 
-	precioFinal=precio-(precio*descuento/100);
+	// precioFinal=precio-(precio*descuento/100);
 
-		document.getElementById('elPrecioFinal').value=precioFinal;
+	// 	document.getElementById('elPrecioFinal').value=precioFinal;
 
 
 
