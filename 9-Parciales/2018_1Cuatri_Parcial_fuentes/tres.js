927604a28fddede7329150e91e5be7fd92ba447d
federicoaner <federicoaner@gmail.com>
2020-02-29 00:45:37 -0300

    ejercicios parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -8,10 +8,20 @@ function mostrar()
 	var cantidadKilos;
 	var marca;
 	var contador=0;
-	var carga=0;
+	var cargaContenedor=0;
+	var maxKilos;
+	var maxMarca;
+	var maxImporte;
+	var maxbolsas;
+	var marcaMasBolsas;
+	var marcaMasImporte;
+	var minKilos;
+	var importeminkilos;
+	var marcaminkilos;
 
+	   
 	
-	while(respuesta=="si")
+	while(respuesta=="si" &&  cargaContenedor<1000 )
 	{
 		contador++;
 		
@@ -19,19 +29,79 @@ function mostrar()
 		marca=prompt("ingrese la marca ");
 
 		kilosxBolsa=prompt("ingrese kilos x bolsa");
-		kilosxBolsa=parseInt=(kilosxBolsa);
-
-		cantidadBolsas=prompt("ingrese cantidad de bolsas")
-
-
-		cantidadkilos=kilosxBolsa*cantidadBolsas
-		carga=carga+cantidadkilos
-		if(carga>1000)
+		kilosxBolsa=parseInt(kilosxBolsa);
+		while(isNaN(kilosxBolsa))
 		{
-			alert("supera la carga maxima");
+			kilosxBolsa=prompt("error reingrese kilos x bolsa");
+			kilosxBolsa=parseInt(kilosxBolsa);
 		}
-	
+
+		cantidadBolsas=prompt("ingrese cantidad de bolsas");
+		cantidadBolsas=parseInt(cantidadBolsas);
+		while(isNaN(cantidadBolsas))
+		{
+		cantidadBolsas=prompt("ingrese cantidad de bolsas");
+		cantidadBolsas=parseInt(cantidadBolsas);
+		}
+
+		cantidadKilos=kilosxBolsa*cantidadBolsas
+		cargaContenedor=cargaContenedor+cantidadKilos
+		
+		if( cargaContenedor>1000)
+		{
+		kilosxBolsa=prompt(" error supera la carga maxima de 1000kilos, reingrese cantidad");
+		kilosxBolsa=parseInt(kilosxBolsa)
+		}
+		
+		importexBolsa=prompt("ingrese el importe x bolsa");
+		importexBolsa=parseInt(importexBolsa);
+		while(isNaN(importexBolsa))
+		{
+		importexBolsa=prompt("ingrese el importe x bolsa");
+		importexBolsa=parseInt(importexBolsa);
+		}
+
 		respuesta=prompt("desea continuar?");
+
+		if(contador==1)
+		{
+			maxKilos=cantidadKilos;
+			maxMarca=marca;
+			maxImporte=importexBolsa;
+			maxbolsas=cantidadBolsas;
+			mayorImporte=importexBolsa;
+			marcaMasImporte=marca;
+			minKilos=cantidadKilos
+		}
+
+
+		if(cantidadKilos>maxKilos)
+		{
+		
+		maxKilos=cantidadKilos;
+		maxMarca=marca;
+		 }
+		
+		else
+		{
+			minKilos=minKilos
+			importeminkilos=importexBolsa;
+			marcaminkilos=marca;
+		}
+
+		if(cantidadBolsas>maxbolsas)
+		{
+			maxbolsas=cantidadBolsas;
+			marcaMasBolsas=marca;
+		}
+
+
+		if(importexBolsa> maxImporte)
+		{
+			maxImporte=importexBolsa;
+			marcaMasImporte=marca;
+
+		}
 
 
 		
@@ -40,7 +110,12 @@ function mostrar()
 
 	}
 
+	console.log(cargaContenedor)
 
+	document.write("la marca con mas kilos es: "+maxMarca+"<br>")
+	document.write("la marca con mas bolsas es: "+marcaMasBolsas+"<br>")
+	document.write("la marca con el mayor importe es : "+marcaMasImporte+"<br>")
+	document.write("el importe de la marca con menos kilos es :  "+importeminkilos+ "y la marca es"+marcaminkilos+"<br>")
 
 
 
